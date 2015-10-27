c07289a144724da78661eeb27fcbf7818e2f9606
Lu Cho-Ching <choching.work@gmail.com>
2015-10-27 18:04:28 +0800

    Create sourcemap file while using babel to transform es6 js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var sourcemaps = require('gulp-sourcemaps');
+var path = require('path');
 
 var paths = {
   es6: 'es6/**/*.js',
-  es5: 'build'
+  es5: 'build',
+  sourceRoot: path.join(__dirname, 'es6')
 };
 
 gulp.task('babel', ()=> 
   gulp
     .src(paths.es6)
+    .pipe(sourcemaps.init())
     .pipe(babel({ 
       whitelist: ['es6.destructuring'] 
     }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(paths.es5))
 );
 
